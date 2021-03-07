6bba845c7494367d62b8881f48af502f353d9287
GradedJestRisk <graded.jest.risk@gmail.com>
2021-03-07 17:18:13 +0100

    Fix skeleton

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const simpleGit = require('simple-git');
-const git = simpleGit();
-const migrationDirectory = core.getInput('migration-directory');
-const errorMessage = 'If merged, this PR  would cause 2 migration files to be shipped together in next release, which may cause a deadlock'
+const errorMessage = 'If merged, this PR would cause 2 migration files to be shipped together in next release, which may cause a deadlock'
 
 const nextReleaseWillContainsMigration = function() {
+   // get last tag
+   // git describe --tags `git rev-list --tags --max-count=1`
+
+   // get diff (files to be released)
+   // git diff --name-only main <TAG>
+
+   // git diff --name-only main $(git describe --tags `git rev-list --tags --max-count=1`)
+
+   // migrations/bar
    return true;
 }
 
 const branchContainsAMigration = function() {
+   // git diff --name-only main
+   // migrations/foobar
    return true;
 }
 
-console.log(`migrationDirectory ${migrationDirectory()}`);
+const migrationDirectory = core.getInput('migration-directory');
+
+console.log(`migrationDirectory ${migrationDirectory}`);
 console.log(`nextReleaseWillContainsMigration ${nextReleaseWillContainsMigration()}`);
 console.log(`branchContainsAMigration ${branchContainsAMigration()}`);
 

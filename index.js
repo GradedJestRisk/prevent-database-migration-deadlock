f12dd1be059b35145e006984ac222e1c1a0861e0
GradedJestRisk <graded.jest.risk@gmail.com>
2021-03-07 18:12:18 +0100

    Use execSync

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const core = require('@actions/core');
+const execSync = require('child_process').execSync;
+
 const errorMessage = 'If merged, this PR would cause 2 migration files to be shipped together in next release, which may cause a deadlock'
-
-const nextReleaseWillContainsMigration = function() {
-   // get last tag
-   // git describe --tags `git rev-list --tags --max-count=1`
-
-   // get diff (files to be released)
-   // git diff --name-only main <TAG>
-
-   // git diff --name-only main $(git describe --tags `git rev-list --tags --max-count=1`)
-
-   // migrations/bar
-   return true;
-}
-
-const branchContainsAMigration = function() {
-   // git diff --name-only main
-   // migrations/foobar
-   return true;
-}
 
 const migrationDirectory = core.getInput('migration-directory');
 
-console.log(`migrationDirectory ${migrationDirectory}`);
-console.log(`nextReleaseWillContainsMigration ${nextReleaseWillContainsMigration()}`);
-console.log(`branchContainsAMigration ${branchContainsAMigration()}`);
+const nextReleaseWillContainsMigration = function() {
 
-if (nextReleaseWillContainsMigration() && branchContainsAMigration()) {
+   const rawCount = execSync('git diff --name-only origin/main $(git describe --tags `git rev-list --tags --max-count=1`) | grep --count ' + migrationDirectory ).toString().trim();
+   const count = parseInt(rawCount);
+   // migrations/bar => 1
+
+   console.log(`In next release: count ${count}`);
+   return (count !== 0);
+}
+
+const branchContainsMigration = function() {
+
+   const rawCount = execSync('git diff --name-only origin/main | grep --count ' + migrationDirectory).toString().trim();
+   // migrations/foobar => 1
+   const count = parseInt(rawCount);
+   
+   console.log(`To be merged migration count: ${count}`);
+   return (count !== 0);
+}
+
+if (nextReleaseWillContainsMigration() && branchContainsMigration()) {
    core.setFailed(errorMessage);
 }
